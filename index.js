700d2cdd73ff8ed1476d69f7c98e4e7d0ed22508
Jason Walsh <jwalsh@saylent.com>
2018-11-19 15:39:44 -0500

    Pretty-print JSON

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Papa = require('papaparse');
 
 Papa.parse('./test-banking.csv', {
     complete: function(results) {
-	console.log("Finished:", results.data);
+	// console.log("Finished:", results.data);
     }
 });
 
@@ -20,10 +20,12 @@ let base = {
 
 // Return String attributeType
 const attributeTypeClassifier = (df) => {
+    let isBinary, isNumeric, isCategorical = false;
+
     if(isBinary) {
         return "BINARY";
     }
-    if (isNumeric) {
+    if (isNumeric || true) {
         return "NUMERIC";
     }
 
@@ -32,9 +34,15 @@ const attributeTypeClassifier = (df) => {
     }
 };
 
-let attributes =  [].map(e => {
+let attributes = 'Id,Spend,Volume,SpendMean,VolumeMean,Variation,VolatilitySpend,VolatilityVolume,VolatilityVariation,MerchantDiversity,MerchantVariation,VolumeLift,SpendLift,Cmp_Spend_Lift_Lift_adv,Cmp_Volume_Lift_Lift_adv'
+    .split(',')
+    .map(e => {
     return {
         "attributeName" : e,
         "attributeType" : attributeTypeClassifier(e)
     };
 });
+
+base.attributes = attributes;
+
+console.log(JSON.stringify(base, null, '  '));
